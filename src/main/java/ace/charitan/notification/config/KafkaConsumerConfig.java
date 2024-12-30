18d405c60d89743c1eecc257cf97c52a57ddc8ea
phucdoan2003 <thienphucdoantran@gmail.com>
2024-12-30 18:18:54 +0700

    Config sync request response for kafka

--- src/main/java/ace/charitan/notification/config/KafkaConsumerConfig.java
+++ src/main/java/ace/charitan/notification/config/KafkaConsumerConfig.java
@@ -2,11 +2,13 @@ package ace.charitan.notification.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
@@ -16,11 +18,14 @@ import java.util.Map;
 @Configuration
 class KafkaConsumerConfig {
 
+    @Autowired
+    private KafkaTemplate<String, Object> kafkaTemplate;
+
     @Bean
     public ConsumerFactory<String, Object> consumerFactory() {
         Map<String, Object> configProps = new HashMap<>();
         configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
-        configProps.put(ConsumerConfig.GROUP_ID_CONFIG, "donation");
+        configProps.put(ConsumerConfig.GROUP_ID_CONFIG, "notification");
         configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
         configProps.put(JsonDeserializer.TRUSTED_PACKAGES, "ace.charitan.*");
@@ -30,6 +35,7 @@ class KafkaConsumerConfig {
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory() {
         ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setReplyTemplate(kafkaTemplate);
         factory.setConsumerFactory(consumerFactory());
         return factory;
     }
--- src/main/java/ace/charitan/notification/external/consumer/KafkaMessageConsumer.java
+++ src/main/java/ace/charitan/notification/external/consumer/KafkaMessageConsumer.java
@@ -2,16 +2,32 @@ package ace.charitan.notification.external.consumer;
 
 
 import ace.charitan.common.dto.TestKafkaMessageDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Service;
+
+import static org.springframework.kafka.support.KafkaHeaders.REPLY_TOPIC;
 
 @Service
 class KafkaMessageConsumer {
 
-    @KafkaListener(topics = "donation-notification", groupId = "donation")
+    @Autowired
+    private KafkaTemplate<String, Object> kafkaTemplate;
+
+    @KafkaListener(topics = "donation-notification", groupId = "notification")
     public void listen(TestKafkaMessageDto dto) {
         System.out.println("Donation microservice received message");
         System.out.println(dto);
+    }
+
+    @KafkaListener(topics = "john-request", groupId = "notification")
+    @SendTo(REPLY_TOPIC)
+    public TestKafkaMessageDto testRequestResponseTemplate(TestKafkaMessageDto dto) {
+        System.out.println("Received John Request: " + dto.toString());
+
+        return new TestKafkaMessageDto("John Response", "Notification response for John Request");
     }
 
 }
